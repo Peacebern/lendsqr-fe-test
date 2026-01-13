6c688e7665b141631f2f6166b915c9eecb61c42d
peacebern <peace8282bern@gmail.com>
2026-01-13 14:15:45 +0100

    feat: build after test and debugging

---
+++ README.md
@@ -0,0 +1,47 @@
+
+# LENDSQR FE CANDIDATES TEST
+
+This project is a Submission for Lendsqr Frontend Engineers Candidates Test. 
+
+
+## Authors
+
+- [@Peacebern](https://github.com/peacebern)
+
+
+## Optimizations
+
+Utilized cust hooks to Improve Perfomance e.g using the useMemo hook to handle the table pagination improved the performance by 100%.
+
+
+## Screenshots
+
+
+
+
+## Appendix
+
+The Project Employs Practices Like:
+
+- Maintain Clear Folder Structure.
+- Institute a Structured Import Order.
+- Adhere To Naming Conventions.
+- Use a Linter.
+- Employ Snippet Libraries.
+- Uses Scss.
+- Limit Component Creation.
+
+
+## Features
+
+
+- Live previews
+- Fullscreen mode
+- Cross platform
+- user management
+
+
+## Tech Stack
+
+**Client:** React, Vite, SCSS, Typescript
+
---
+++ src/components/table/ActionButton.tsx
@@ -0,0 +1,53 @@
+import actbtn from "assets/figma/actbtn.svg";
+import { MouseEventHandler, useRef, useState } from "react";
+import view from "assets/figma/eye.svg";
+import blc_usr from "assets/figma/blacklistuser.svg";
+import act_usr from "assets/figma/activateuser.svg";
+import { FaEdit } from "react-icons/fa";
+import useOnClickOutside from 'hooks/useOnClickOutside.tsx';
+
+interface Props {
+  id: string;
+  saveSelectedUserIdStorage: MouseEventHandler<HTMLSpanElement>;
+}
+const ActionButton = ({ id, saveSelectedUserIdStorage }: Props) => {
+  const [open, setOpen] = useState(false);
+
+  const tableActionBtnRef = useRef<HTMLDivElement>(null);
+  const handleClickOutside = (event: any) => {
+    setOpen(false);
+  };
+
+  // hook to handle when user clicks outside the popup menu
+  // this means that for users to close the popup they must
+  // click outside the menu
+  useOnClickOutside(tableActionBtnRef, handleClickOutside);
+  return (
+    <>
+      <button className="action-btn" onClick={() => setOpen(!open)}>
+        <img src={actbtn} alt="action_button" />
+      </button>
+      <button className="action-btn-sx" onClick={() => setOpen(!open)}>
+        <FaEdit />
+      </button>
+      {open && (
+        <div className="table-action-menu" ref={tableActionBtnRef}>
+          <span onClick={saveSelectedUserIdStorage}>
+            <img src={view} alt="eye_icon" />
+            View Details
+          </span>
+          <span>
+            <img src={blc_usr} alt="user_cancel_icon" />
+            Blacklist User
+          </span>
+          <span>
+            <img src={act_usr} alt="user_check_user" />
+            Activate User
+          </span>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ActionButton;
