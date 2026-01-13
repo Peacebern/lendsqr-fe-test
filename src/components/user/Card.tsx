a9ca62ade314e6609e0e37955480014cbedd4941
peacebern <peace8282bern@gmail.com>
2026-01-13 14:04:23 +0100

    feat: create user card components for display. Card.tsx base card component Build UserCard.tsx for user profile cards Create Dropdown.tsx for user action menus

---
+++ src/components/user/Card.tsx
@@ -0,0 +1,26 @@
+import { formatNumberWithCommas } from "utils/index.tsx";
+
+interface Props {
+    /**image or icon to render on card: 40px */
+    icon?: string;
+    /**name or category of card */
+    name: string;
+    /**numbers */
+    fig: number;
+    /**id of component */
+    id: string;
+}
+
+const Card = (props: Props) => {
+  return (
+    <div className="card" id={props.id}>
+        <span className='icon'>
+          <img src={props.icon} alt={`${props.icon}_icon`}/>
+        </span>
+        <span className='name'>{props.name}</span>
+        <span className='fig'>{formatNumberWithCommas(props.fig)}</span>
+    </div>
+  )
+}
+
+export default Card
