ac332b23e32a1df22b40eda3a2bd0aa9be8c2bb9
peacebern <peace8282bern@gmail.com>
2026-01-13 13:23:28 +0100

    feat: setup routing structure for main pages, feat: add error boundary and auth context provider

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+
+import "./App.scss";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "pages/Login.tsx";
+import Users from "pages/Users.tsx";
+import UserDetails from "pages/UserDetails.tsx";
+import Layout from "layout/Layout.tsx";
+
+function App() {
+  return (
+    <Routes>
+      {/* Redirect root to login */}
+      <Route path="/" element={<Navigate to="/users" replace />} />
+
+      {/* Login page WITHOUT sidebar & navbar */}
+      <Route path="/login" element={<Login />} />
+
+      {/* All pages WITH sidebar & navbar */}
+      <Route element={<Layout />}>
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
