7acb0cd9dea3cc82feb3b1b7e036c52dfc4ab2f3
peacebern <peace8282bern@gmail.com>
2026-01-13 20:01:46 +0100

    search button positioning fix

--- src/components/header/SearchInput.tsx
+++ src/components/header/SearchInput.tsx
@@ -6,7 +6,7 @@ const SearchInput = () => {
     <div className="search" data-testid="search-input">
       <input type="text" name="search" id="srch" placeholder="Search for anything" style={{ opacity: 0.70, color: '#545F7D', fontSize: 14, fontFamily: 'Work Sans', fontWeight: '400', wordWrap: 'break-word' }} />
       <button>
-        <img src={searchicon} alt="search_icon" />
+        <img src={searchicon} alt="search_icon" className="searchImage" />
       </button>
     </div>
   )
