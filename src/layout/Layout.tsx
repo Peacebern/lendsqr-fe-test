d7367527816b693db77908c224397414b8e342d8
peacebern <peace8282bern@gmail.com>
2026-01-13 18:36:29 +0100

    removing unnecessary commented  out codes

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,21 +1,3 @@
-// import { ReactNode } from 'react'
-// import Header from '../components/header/Header.tsx'
-// import Sidebar from '../components/sidebar/Sidebar.tsx'
-
-// interface Props {
-//     children: ReactNode;
-// }
-// const Layout = (props: Props) => {
-//   return (
-//     <div className="layout">
-//         <Header/>
-//         <Sidebar/>
-//         <main>{props.children}</main>
-//     </div>
-//   )
-// }
-
-// export default Layout
 
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header.tsx";
